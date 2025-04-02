b7eb11cc3e6c81a49912fd2c0f7b7eea93a5999a
Nazmul <120049738+mnhasan22@users.noreply.github.com>
2025-04-02 10:30:14 -0400

    Add Post model and API view for posts

--- NAD_A4/urls.py
+++ NAD_A4/urls.py
@@ -1,1 +1,9 @@
-ECHO is on.
+from django.contrib import admin
+from django.urls import path
+from posts.views import post_list  # Corrected import
+from django.http import HttpResponse
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', post_list, name='post_list'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def post_list(request):
+    return HttpResponse("Hello, this is the post list view.")
